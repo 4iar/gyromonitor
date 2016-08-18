e812438d622cf5a192f5c9bde7c87e8ba291d76d
4iar <4iar@users.noreply.github.com>
2016-08-18 22:20:14 +0100

    Add client function to acquire orientation via bluetooth

--- desktop/communication/bluetooth_method.py
+++ desktop/communication/bluetooth_method.py
@@ -5,6 +5,7 @@ class Bluetooth():
     def __init__(self, target_addr, port=1):
         self.target_addr = target_addr
         self.port = port
+        self.sock = None
 
     def connect(self):
         self.sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -18,3 +19,21 @@ class Bluetooth():
 
     def disconnect(self):
         self.sock.close()
+
+    def get_orientation(self):
+        if not self.sock:
+            print("""
+            connection not established:
+            establish a connection before polling
+            """)
+            return
+
+        self.sock.send('GET_ORIENTATION')
+
+        orientation = int(self.sock.recv(1024))
+
+
+        if orientation in [0, 90, 180, 270]:
+            return orientation
+        else:
+            print("got invalid orientation: {}".format(orientation))
