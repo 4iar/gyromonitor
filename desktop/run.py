8f1a35ec5b536d81f96cbbd34226225a389524ab
4iar <4iar@users.noreply.github.com>
2016-08-19 23:57:02 +0100

    Add placeholder desktop run script

---
+++ desktop/run.py
@@ -0,0 +1,24 @@
+# placeholder imports
+from .communication.bluetooth_client import Bluetooth
+from .orientation.linux import Monitor
+from time import sleep
+
+# begin talking to the pi
+# poll for curr orientation
+
+
+monitor = Monitor()
+target_addr = input('bd_addr: ')
+conn = Bluetooth(target_addr)
+conn.connect()
+
+prev_orientation = conn.get_orientation()
+while True:
+    sleep(0.5)
+    orientation = conn.get_orientation()
+    if orientation != prev_orientation:
+        Monitor.change_orientation(orientation)
+        prev_orientation = orientation
+
+
+
