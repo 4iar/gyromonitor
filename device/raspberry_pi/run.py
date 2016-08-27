c23ae1ac360b8ee4cd685ba21a91a2f8afdf3afa
4iar <4iar@users.noreply.github.com>
2016-08-28 00:50:15 +0100

    Prevent connection from blocking orientation acquisition

--- device/raspberry_pi/run.py
+++ device/raspberry_pi/run.py
@@ -1,13 +1,16 @@
 from communication.bluetooth_server import Bluetooth
 from sensors import SenseHat
 from time import sleep
+import threading
 
 
 sensor = SenseHat.SenseHat()
 
 conn = Bluetooth()
 conn.orientation = sensor.get_orientation()
-conn.listen()
+
+t = threading.Thread(target=conn.listen)
+t.start()
 
 while True:
     sleep(0.5)
