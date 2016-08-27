b4b21d3c559f6a703d9a8421bacbaf9f18fb945b
4iar <4iar@users.noreply.github.com>
2016-08-27 23:29:11 +0100

    Improve handling of ambiguous yaw values

--- device/raspberry_pi/sensors/SenseHat.py
+++ device/raspberry_pi/sensors/SenseHat.py
@@ -5,4 +5,15 @@ class SenseHat():
         self.sense = SenseHatDriver()
 
     def get_orientation(self):
-        return self.sense.get_orientation()['yaw']
+        raw_orientation = self.sense.get_orientation()['yaw']
+
+        for eps in [10, 25, 90]:
+            orientation = self.round_orientation(raw_orientation, eps)
+            if orientation:
+                return orientation
+
+    def round_orientation(self, raw_orientation, epsilon):
+        for o in [0, 90, 180, 270]:
+            if epsilon >= abs(o - raw_orientation) >= 0:
+                return o
+
