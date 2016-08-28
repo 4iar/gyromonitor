0b7c06b9569cf44317c99d681197d72238db7d0c
4iar <4iar@users.noreply.github.com>
2016-08-28 01:36:27 +0100

    Change method for orientation detection

--- device/raspberry_pi/sensors/SenseHat.py
+++ device/raspberry_pi/sensors/SenseHat.py
@@ -1,20 +1,20 @@
 from sense_hat import SenseHat as SenseHatDriver
 
+
 class SenseHat():
     def __init__(self):
         self.sense = SenseHatDriver()
         self.sense.set_imu_config(False, True, False)  # we only need the gyroscope
 
     def get_orientation(self):
-        raw_orientation = self.sense.get_orientation()['yaw']
-
-        for eps in [10, 25, 90]:
-            orientation = self.round_orientation(raw_orientation, eps)
-            if orientation:
-                return orientation
-
-    def round_orientation(self, raw_orientation, epsilon):
-        for o in [0, 90, 180, 270]:
-            if epsilon >= abs(o - raw_orientation) >= 0:
-                return o
-
+        x, y, z = self.sense.get_accelerometer_raw().values()
+        x = round(x, 0)
+        y = round(y, 0)
+        if x == -1:
+            return 180
+        elif y == 1:
+            return 90
+        elif y == -1:
+            return 270
+        else:
+            return 0
